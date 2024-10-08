f77f986436d5613922ec3c32d9f6f069a8b73aa2
DOH-7 CDC - Esguerra <doh7cdc.dev@gmail.com>
2024-10-08 09:30:01 +0800

    changed connection to pool

--- src/connections/HybridConnection.ts
+++ src/connections/HybridConnection.ts
@@ -4,34 +4,24 @@ import fs from "fs";
 
 dotenv.config();
 
-const connectToDatabase = async () => {
-  // check if production or not
-  const isProduction: boolean =
-    process.env.IS_PRODUCTION?.toLowerCase() === "true";
+const createDatabasePool = async () => {
+  // Check if in production or not
+  const isProduction = process.env.IS_PRODUCTION?.toLowerCase() === "true";
 
-  let db: mysql.Connection;
+  const pool = mysql.createPool({
+    host: isProduction ? process.env.PROD_HOSTNAME : process.env.DEV_HOSTNAME,
+    user: isProduction ? process.env.PROD_HYBRID_USER : process.env.DEV_HYBRID_USER,
+    password: isProduction ? process.env.PROD_HYBRID_PASS : process.env.DEV_HYBRID_PASS,
+    database: process.env.DATABASE_NAME,
+    port: isProduction ? Number(process.env.REMOTE_MYSQL_PORT) : Number(process.env.LOCAL_MYSQL_PORT),
+    ssl: isProduction ? { ca: fs.readFileSync(String(process.env.CA_CERTIFICATE_PATH)) } : undefined,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust as needed
+    queueLimit: 0,
+  });
 
-  if (!isProduction) {
-    db = await mysql.createConnection({
-      host: process.env.DEV_HOSTNAME,
-      user: process.env.DEV_HYBRID_USER,
-      password: process.env.DEV_HYBRID_PASS,
-      database: process.env.DATABASE_NAME,
-      port: Number(process.env.LOCAL_MYSQL_PORT),
-    });
-  } else {
-    db = await mysql.createConnection({
-      host: process.env.PROD_HOSTNAME,
-      user: process.env.PROD_HYBRID_USER,
-      password: process.env.PROD_HYBRID_PASS,
-      database: process.env.DATABASE_NAME,
-      port: Number(process.env.REMOTE_MYSQL_PORT),
-      ssl: { ca: fs.readFileSync(String(process.env.CA_CERTIFICATE_PATH)) },
-    });
-  }
-
-  return db;
+  return pool;
 };
 
-const dbPromise = connectToDatabase();
-export default dbPromise;
+const dbPool = createDatabasePool();
+export default dbPool;
--- src/controllers/record-based/record-controllers/risk-assessment-form/RecordCreationController.ts
+++ src/controllers/record-based/record-controllers/risk-assessment-form/RecordCreationController.ts
@@ -1,10 +1,10 @@
 // all record-creation and insertion functions here:
-export { officerCreateArd } from "./record-controllers/ArdControllers";
-export { officerCreateConsultation } from "./record-controllers/ConsultationLogControllers";
-export { officerCreateFamilyHistory } from "./record-controllers/FamilyHistoryControllers";
-export { officerCreateManagement } from "./record-controllers/ManagementControllers";
-export { officerCreateNcdRiskFactors } from "./record-controllers/NcdRiskFactorsControllers";
-export { officerCreatePastMedicalHistory } from "./record-controllers/PastMedicalHistoryControllers";
-export { officerCreatePatientInfo } from "./record-controllers/PatientInfoControllers";
-export { officerCreateReferral } from "./record-controllers/ReferralControllers";
-export { officerCreateRiskScreening } from "./record-controllers/RiskScreeningControllers";
+export { officerCreateArd } from "./records/ArdControllers";
+export { officerCreateConsultation } from "./records/ConsultationLogControllers";
+export { officerCreateFamilyHistory } from "./records/FamilyHistoryControllers";
+export { officerCreateManagement } from "./records/ManagementControllers";
+export { officerCreateNcdRiskFactors } from "./records/NcdRiskFactorsControllers";
+export { officerCreatePastMedicalHistory } from "./records/PastMedicalHistoryControllers";
+export { officerCreatePatientInfo } from "./records/PatientInfoControllers";
+export { officerCreateReferral } from "./records/ReferralControllers";
+export { officerCreateRiskScreening } from "./records/RiskScreeningControllers";
